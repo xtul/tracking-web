6fce8d62391b0ce11929eab5098b95560c088f58
Michał Piasecki <michal@elexpress.pl>
2021-04-10 23:05:12 +0200

    Added Brotli compression. Added VueLayers plugin.

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,26 @@
 const path = require('path');
+const zlib = require('zlib');
 
 module.exports = {
-    chainWebpack: config => {
-        config.resolve.alias.set(
-            'vue$',
-            path.resolve(__dirname, 'node_modules/vue/dist/vue.esm.js')
-        );
-        {
-            {
-                [
-                    { sideEffects: false }
-                ]
-            }
-        }
-    }
+	chainWebpack: config => {
+		config.resolve.alias.set(
+			'vue$',
+			path.resolve(__dirname, 'node_modules/vue/dist/vue.esm.js')
+		);	
+	},
+	pluginOptions: {
+		compression:{
+			brotli: {
+				filename: '[file].br[query]',
+				algorithm: 'brotliCompress',
+				include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+				compressionOptions: {
+					params: {
+						[zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+					},
+				},
+				minRatio: 0.8,
+			}
+		}
+	}	
 }
